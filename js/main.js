bbb5cd045d24b1697ff0f077eaaceeb2a9d8f2e0
Rutuja Chandgude <74643325+rutuchandgude@users.noreply.github.com>
2021-09-07 22:04:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+const responsive={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
 $(document).ready(function(){
 $nav=$('.nav');
 $toggleCollapse=$('.toggle-collapse');
@@ -14,7 +28,18 @@ $('.owl-carousel').owlCarousel({
     autoplayTimeout:3000,
     dots:false,
     nav:true,
-    navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+    responsive:responsive
 });
+
+//click to scroll top
+$('.move-up span').click(function(){
+    $('html, body').animate({
+        scrollTop:0
+    },1000);
+})
+
+//AOS(Animation on scroll) scroll
+AOS.init();
 
 });
